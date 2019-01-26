ab8515a9798bdf5db2cae47b9078a594d4af8e06
Jawad Mahmoud <jd.mahmoud@gmail.com>
2019-01-26 22:45:33 +0000

    Bug fixes with cookies

--- js/Functionality/settings.js
+++ js/Functionality/settings.js
@@ -33,6 +33,32 @@ var timeSwitch = 2;
 var twoSwitches = true;
 var wordlist = [''];
 
+function chooseWordList() {
+    if (document.getElementById('words_input').checked) {
+        wordlist = ['bag', 'bed', 'bin', 'box', 'can', 'cap', 'cat', 'dog', 'fan', 'fox', 'hat', 'hen', 'jam', 'jug', 'leg', 'man', 'map', 'men', 'mug', 'net', 'pan', 'peg', 'pen', 'pig', 'rat', 'red', 'sun', 'tap', 'ten', 'van', 'web', 'zip'];
+        setCookie("GameTypeCookie","words_input",2);
+
+    } else if (document.getElementById('blends_input').checked) {
+        wordlist = ['ant', 'belt', 'brush', 'bulb', 'clap', 'crab', 'desk', 'drum', 'fist', 'flag', 'flask', 'frog', 'gift', 'golf', 'green', 'hand', 'lamp', 'milk', 'nest', 'plug', 'pram', 'raft', 'shelf', 'spoon', 'stamp', 'stop', 'string', 'swim', 'swing', 'tent', 'tree'];
+        setCookie("GameTypeCookie","blends_input",2);
+
+    } else if (document.getElementById('digraphs_input').checked) {
+        wordlist = ['bath', 'bee', 'book', 'boot', 'chin', 'cook', 'feet', 'fish', 'food', 'foot', 'good', 'heel', 'hood', 'hoof', 'hook', 'jeep', 'king', 'loop', 'moon', 'path', 'peek', 'peel', 'pool', 'queen', 'ring', 'roof', 'ship', 'string', 'swing', 'weed', 'wood', 'wool'];
+        setCookie("GameTypeCookie","digraphs_input",2);
+
+    } else if (document.getElementById('moredigraphs_input').checked) {
+        wordlist = ['car', 'chair', 'chick', 'cloud', 'coat', 'coin', 'cue', 'dart', 'egg', 'fork', 'goat', 'hair', 'horn', 'ill', 'jar', 'kerb', 'mouth', 'nail', 'oil', 'pie', 'pink', 'rain', 'road', 'shark', 'shell', 'snail', 'sock', 'star', 'tail', 'tank', 'tie', 'train'];
+        setCookie("GameTypeCookie","moredigraphs_input",2);
+
+    } else {
+        wordlist = [''];
+        setCookie("GameTypeCookie","",2);
+
+    }
+
+    sessionStorage.setItem("wordlist", wordlist);
+};
+
 function getSettingsValues() {
 
     input_cookie_in = getCookie("InputCookie");
@@ -77,6 +103,11 @@ function getSettingsValues() {
     game_type_cookie = getCookie("GameTypeCookie");
     if(game_type_cookie !== "") {
         $("#"+game_type_cookie).prop('checked',true);
+        chooseWordList();
+        console.log("Yes Cookie choose word list");
+    } else {
+        chooseWordList();
+        console.log("No Cookie choose word list");
     }
 
 }
@@ -183,38 +214,13 @@ function demoSettings() {
 
     $("#dwInfoButton").show();
 
-    //set default wordlist on load of this webpage
-    wordlist = ['bag', 'bed', 'bin', 'box', 'can', 'cap', 'cat', 'dog', 'fan', 'fox', 'hat', 'hen', 'jam', 'jug', 'leg', 'man', 'map', 'men', 'mug', 'net', 'pan', 'peg', 'pen', 'pig', 'rat', 'red', 'sun', 'tap', 'ten', 'van', 'web', 'zip'];
-    sessionStorage.setItem("wordlist", wordlist);
-
 
     //if there is a click on to indicate change of game/activity type
     // get the appropriate wordlist for it
-    document.getElementById('game_type').onclick = function () {
-        if (document.getElementById('words_input').checked) {
-            wordlist = ['bag', 'bed', 'bin', 'box', 'can', 'cap', 'cat', 'dog', 'fan', 'fox', 'hat', 'hen', 'jam', 'jug', 'leg', 'man', 'map', 'men', 'mug', 'net', 'pan', 'peg', 'pen', 'pig', 'rat', 'red', 'sun', 'tap', 'ten', 'van', 'web', 'zip'];
-            setCookie("GameTypeCookie","words_input",2);
-
-        } else if (document.getElementById('blends_input').checked) {
-            wordlist = ['ant', 'belt', 'brush', 'bulb', 'clap', 'crab', 'desk', 'drum', 'fist', 'flag', 'flask', 'frog', 'gift', 'golf', 'green', 'hand', 'lamp', 'milk', 'nest', 'plug', 'pram', 'raft', 'shelf', 'spoon', 'stamp', 'stop', 'string', 'swim', 'swing', 'tent', 'tree'];
-            setCookie("GameTypeCookie","blends_input",2);
-
-        } else if (document.getElementById('digraphs_input').checked) {
-            wordlist = ['bath', 'bee', 'book', 'boot', 'chin', 'cook', 'feet', 'fish', 'food', 'foot', 'good', 'heel', 'hood', 'hoof', 'hook', 'jeep', 'king', 'loop', 'moon', 'path', 'peek', 'peel', 'pool', 'queen', 'ring', 'roof', 'ship', 'string', 'swing', 'weed', 'wood', 'wool'];
-            setCookie("GameTypeCookie","digraphs_input",2);
-
-        } else if (document.getElementById('moredigraphs_input').checked) {
-            wordlist = ['car', 'chair', 'chick', 'cloud', 'coat', 'coin', 'cue', 'dart', 'egg', 'fork', 'goat', 'hair', 'horn', 'ill', 'jar', 'kerb', 'mouth', 'nail', 'oil', 'pie', 'pink', 'rain', 'road', 'shark', 'shell', 'snail', 'sock', 'star', 'tail', 'tank', 'tie', 'train'];
-            setCookie("GameTypeCookie","moredigraphs_input",2);
-
-        } else {
-            wordlist = [''];
-            setCookie("GameTypeCookie","",2);
-
-        }
-
-        sessionStorage.setItem("wordlist", wordlist);
-    };
+    $("#game_type").click(function() {
+        chooseWordList();
+    });
+    
 
 
 }
